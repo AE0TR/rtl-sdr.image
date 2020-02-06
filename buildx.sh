24e7659bb89b975cb8affea13e6866a1e9bae8ef
Dennis Dunn <ansofive@gmail.com>
2020-02-05 19:36:29 -0700

    Updated the build script with some usage info.

--- buildx.sh
+++ buildx.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
+if [ -z "$1" ]; then 
+    echo "usage: $0 arm_build_node ($0 tcp://raspberrypi.local:2376)"
+    exit
+fi
+
+ARM_NODE=$1
 
 # create a builder
 docker buildx create --name multibuild
 
 # add a node to the builder
-docker buildx create --name multibuild --append tcp://antenna.local:2376
+docker buildx create --name multibuild --append $ARM_NODE
 docker buildx create --name multibuild --node multibuild1 --platform linux/arm
 
 # start the nodes
